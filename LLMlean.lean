223d1e146ff3ca273c0d832624da8d38ca7c5f97
KunihikoK3 <n_ramsamooj@hotmail.com>
2024-05-23 04:48:12 -0400

    LLMlean_example

--- LLMlean.lean
+++ LLMlean.lean
@@ -1,2 +1,13 @@
 import LLMlean.LLMstep
 import LLMlean.LLMqed
+import Mathlib.Data.Real.Basic
+
+def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, f x ≤ a
+
+def FnLb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, a ≤ f x
+
+example (hfa : FnUb f a) (hgb : FnUb g b) : FnUb (fun x ↦ f x + g x) (a + b) := by
+intro x
+calc f x + g x ≤ a + b := add_le_add (hfa _) (hgb _)
