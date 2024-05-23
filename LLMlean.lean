00155d7ef3d9a37bb094ca2020e006c574c7733a
KunihikoK3 <n_ramsamooj@hotmail.com>
2024-05-23 06:00:36 -0400

    LeanCopilot added

--- LLMlean.lean
+++ LLMlean.lean
@@ -1,5 +1,6 @@
 import LLMlean.LLMstep
 import LLMlean.LLMqed
+import LeanCopilot
 import Mathlib.Data.Real.Basic
 
 def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
@@ -10,4 +11,10 @@ def FnLb (f : ℝ → ℝ) (a : ℝ) : Prop :=
 
 example (hfa : FnUb f a) (hgb : FnUb g b) : FnUb (fun x ↦ f x + g x) (a + b) := by
 intro x
-calc f x + g x ≤ a + b := add_le_add (hfa _) (hgb _)
+calc f x + g x ≤ a + b := by exact add_le_add (hfa x) (hgb x)
+
+
+example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a) :
+    FnUb (fun x ↦ f x * g x) (a * b) := by
+intro x
+llmstep ""
--- lakefile.lean
+++ lakefile.lean
@@ -2,12 +2,15 @@ import Lake
 open Lake DSL
 
 package «llmlean» {
-  -- add any package configuration options here
+  moreLinkArgs := #[
+    "-L./.lake/packages/LeanCopilot/.lake/build/lib",
+    "-lctranslate2"
+  ]
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.2.0"
 @[default_target]
 lean_lib «LLMlean» {
   -- add any library configuration options here
