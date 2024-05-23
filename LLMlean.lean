82fc0e7d2de97840747620da53774ecd203fc80b
KunihikoK3 <n_ramsamooj@hotmail.com>
2024-05-23 06:08:14 -0400

    message

--- LLMlean.lean
+++ LLMlean.lean
@@ -11,7 +11,10 @@ def FnLb (f : ℝ → ℝ) (a : ℝ) : Prop :=
 
 example (hfa : FnUb f a) (hgb : FnUb g b) : FnUb (fun x ↦ f x + g x) (a + b) := by
 intro x
-calc f x + g x ≤ a + b := by exact add_le_add (hfa x) (hgb x)
+show f x + g x ≤ a + b
+apply add_le_add
+exact hfa x
+exact hgb x
 
 
 example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a) :
